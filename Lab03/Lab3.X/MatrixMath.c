5fecd27093a1d9a7558a4d91797291d482b3a125
Vara Neha Madem <varamadem@eduroam-169-233-184-75.ucsc.edu>
2022-10-13 19:21:13 -0700

    second commit

---
+++ Lab03/Lab3.X/MatrixMath.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   MatrixMath.c
+ * Author: varamadem
+ *
+ * Created on October 13, 2022, 5:28 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
