bb4bb8d9b69fcd1ccf5f5fdb7cf6fdc78ac9604d
Vara Neha Madem <varamadem@Varas-MacBook-Pro.local>
2022-10-26 22:31:31 -0700

    first commit

---
+++ Lab05/Lab5.X/LinkedList.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   LinkedList.c
+ * Author: varamadem
+ *
+ * Created on October 25, 2022, 4:23 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
